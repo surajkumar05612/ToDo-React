7e90ac354679b882f19ef0921ae9a2839517af1e
Suraj Kumar Jena <73339517+surajkumar05612@users.noreply.github.com>
2022-06-26 16:35:58 +0530

    todo list created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Form from './component/Form.js';
+import TodoList from './component/TodoList.js';
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      
+      <header>
+        ToDo List App By Suraj
+      </header>
+      <Form 
+      inputText={inputText}
+      todos={todos} 
+      setTodos={setTodos} 
+      setInputText={ setInputText }/>
+      <TodoList setTodos={setTodos} todos={todos}/>
     </div>
   );
 }
